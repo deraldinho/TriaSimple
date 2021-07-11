31aa8f112d3250452f3f02472a3aa21cd96083e6
deraldinho <deraldinhopalomino@gmail.com>
2021-07-11 15:44:10 -0300

    Informações de compilação

---
+++ cmake-build-debug/CMakeFiles/TriaSimple.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TriaSimple.dir/main.c.obj"
+  "TriaSimple.exe"
+  "TriaSimple.exe.manifest"
+  "TriaSimple.lib"
+  "TriaSimple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TriaSimple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
